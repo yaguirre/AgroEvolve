b78a242245271707c50b77017dd3a123b5a965c5
Santiago Baena Rivera <sbaenar1@eafit.edu.co>
2018-04-28 10:36:05 -0500

    Conexión en progreso

--- AgroEvolve/app/src/main/java/com/example/user/agroevolve/Conectar/Conexion.java
+++ AgroEvolve/app/src/main/java/com/example/user/agroevolve/Conectar/Conexion.java
@@ -1,18 +1,100 @@
 package com.example.user.agroevolve.Conectar;
-import java.net.InetAddress;
-import java.net.Socket;
+
+import java.net.HttpURLConnection;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.io.File;
 
 public class Conexion {
-    public static void main(String[] args) {
-        try {
-            InetAddress addr;
-            Socket sock = new Socket("http://159.65.225.153/api/guess-what/?", 80);
-            addr = sock.getInetAddress();
-            System.out.println("Connected to " + addr);
-            sock.close();
-        } catch (java.io.IOException e) {
-            System.out.println("Can't connect to " + args[0]);
-            System.out.println(e);
+
+    public static void main(){
+
+    }
+
+    public Conexion() {
+    }
+
+    public void crearconexion(){
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        DataInputStream inStream = null;
+        String lineEnd = "\r\n";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1*1024*1024;
+        String urlString = "http://159.65.225.153/api/guess-what/";   // server ip
+        try
+        {
+            //------------------ CLIENT REQUEST
+            FileInputStream fileInputStream = new FileInputStream(new File(path) );
+            // open a URL connection to the Servlet
+            URL url = new URL(urlString);
+            // Open a HTTP connection to the URL
+            conn = (HttpURLConnection) url.openConnection();
+            // Allow Inputs
+            conn.setDoInput(true);
+            // Allow Outputs
+            conn.setDoOutput(true);
+            // Don't use a cached copy.
+            conn.setUseCaches(false);
+            // Use a post method.
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+"    ");
+            dos = new DataOutputStream( conn.getOutputStream() );
+            dos.writeBytes(lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + path + "\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+
+            // create a buffer of maximum size
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            // read file and write it into form...
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            while (bytesRead > 0)
+            {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            // send multipart form data necesssary after file data...
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(lineEnd);
+
+            // close streams
+            Log.e("Debug","File is written");
+            fileInputStream.close();
+            dos.flush();
+            dos.close();
         }
+        catch (MalformedURLException ex)
+        {
+            Log.e("Debug", "error: " + ex.getMessage(), ex);
+        }
+        catch (IOException ioe)
+        {
+            Log.e("Debug", "error: " + ioe.getMessage(), ioe);
+        }
+
+        //------------------ read the SERVER RESPONSE
+        try {
+            inStream = new DataInputStream ( conn.getInputStream() );
+            String str;
+            while (( str = inStream.readLine()) != null)
+            {
+                Log.e("Debug","Server Response "+str);
+            }
+            inStream.close();
+        }
+        catch (IOException ioex){
+            Log.e("Debug", "error: " + ioex.getMessage(), ioex);
+        }
+    }
     }
 }
