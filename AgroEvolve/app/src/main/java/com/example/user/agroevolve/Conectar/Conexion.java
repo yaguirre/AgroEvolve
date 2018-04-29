da67fb28bc91b2fa8606556cd255933784f0be41
Santiago Baena Rivera <sbaenar1@eafit.edu.co>
2018-04-28 22:30:58 -0500

    Cambios en la forma de conexion

--- AgroEvolve/app/src/main/java/com/example/user/agroevolve/Conectar/Conexion.java
+++ AgroEvolve/app/src/main/java/com/example/user/agroevolve/Conectar/Conexion.java
@@ -1,98 +1,60 @@
 package com.example.user.agroevolve.Conectar;
 
-import android.util.Log;
-
-import java.net.HttpURLConnection;
-import java.io.DataOutputStream;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.net.URL;
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
-
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Conexion {
 
-    public void crearconexion(){
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        DataInputStream inStream = null;
-        String lineEnd = "\r\n";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1*1024*1024;
-        String urlString = "http://159.65.225.153/api/guess-what/";   // server ip
-        try
-        {
-            //------------------ CLIENT REQUEST
-            FileInputStream fileInputStream = new FileInputStream(new File("") );
-            // open a URL connection to the Servlet
-            URL url = new URL(urlString);
-            // Open a HTTP connection to the URL
-            conn = (HttpURLConnection) url.openConnection();
-            // Allow Inputs
-            conn.setDoInput(true);
-            // Allow Outputs
-            conn.setDoOutput(true);
-            // Don't use a cached copy.
-            conn.setUseCaches(false);
-            // Use a post method.
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+"    ");
-            dos = new DataOutputStream( conn.getOutputStream() );
-            dos.writeBytes(lineEnd);
-            dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + "" + "\"" + lineEnd);
-            dos.writeBytes(lineEnd);
+        private static final String USER_AGENT = "Mozilla/5.0";
 
-            // create a buffer of maximum size
-            bytesAvailable = fileInputStream.available();
-            bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            buffer = new byte[bufferSize];
+        private static final String POST_URL = "http://159.65.225.153/api/guess-what/";
 
-            // read file and write it into form...
-            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            while (bytesRead > 0)
-            {
-                dos.write(buffer, 0, bufferSize);
-                bytesAvailable = fileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+        private static final String POST_PARAMS = "u";
+
+        public static void main(String[] args) throws IOException {
+
+            sendPOST();
+            System.out.println("POST DONE");
+        }
+
+
+
+        private static void sendPOST() throws IOException {
+            URL obj = new URL(POST_URL);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            // For POST only - START
+            con.setDoOutput(true);
+            OutputStream os = con.getOutputStream();
+            os.write(POST_PARAMS.getBytes());
+            os.flush();
+            os.close();
+            // For POST only - END
+
+            int responseCode = con.getResponseCode();
+            System.out.println("POST Response Code :: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) { //success
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // print result
+                System.out.println(response.toString());
+            } else {
+                System.out.println("POST request not worked");
             }
-
-            // send multipart form data necesssary after file data...
-            dos.writeBytes(lineEnd);
-            dos.writeBytes(lineEnd);
-
-            // close streams
-            Log.e("Debug","File is written");
-            fileInputStream.close();
-            dos.flush();
-            dos.close();
         }
-        catch (MalformedURLException ex)
-        {
-            Log.e("Debug", "error: " + ex.getMessage(), ex);
-        }
-        catch (IOException ioe)
-        {
-            Log.e("Debug", "error: " + ioe.getMessage(), ioe);
-        }
-
-        //------------------ read the SERVER RESPONSE
-        try {
-            inStream = new DataInputStream ( conn.getInputStream() );
-            String str;
-            while (( str = inStream.readLine()) != null)
-            {
-                Log.e("Debug","Server Response "+str);
-            }
-            inStream.close();
-        }
-        catch (IOException ioex){
-            Log.e("Debug", "error: " + ioex.getMessage(), ioex);
-        }
-    }
-    }
 }
