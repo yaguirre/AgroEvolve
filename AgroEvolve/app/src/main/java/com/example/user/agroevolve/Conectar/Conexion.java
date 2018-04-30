c7c0b7352a34dc775cea8e0fb166104f5c702f50
Santiago Baena Rivera <sbaenar1@eafit.edu.co>
2018-04-29 19:05:13 -0500

    Conexion completada, Resta probar

--- AgroEvolve/app/src/main/java/com/example/user/agroevolve/Conectar/Conexion.java
+++ AgroEvolve/app/src/main/java/com/example/user/agroevolve/Conectar/Conexion.java
@@ -9,52 +9,52 @@ import java.net.URL;
 
 public class Conexion {
 
-        private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String USER_AGENT = "Mozilla/5.0";
 
-        private static final String POST_URL = "http://159.65.225.153/api/guess-what/";
+    private static final String POST_URL = "http://159.65.225.153/api/guess-what/";
 
-        private static final String POST_PARAMS = "u";
+    private static final String POST_PARAMS = "/Users/santiagob/Downloads/flower-221.jpg";
 
-        public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-            sendPOST();
-            System.out.println("POST DONE");
-        }
+        sendPOST();
+        System.out.println("POST DONE");
+    }
 
 
 
-        private static void sendPOST() throws IOException {
-            URL obj = new URL(POST_URL);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("POST");
-            con.setRequestProperty("User-Agent", USER_AGENT);
+    private static void sendPOST() throws IOException {
+        URL obj = new URL(POST_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
 
-            // For POST only - START
-            con.setDoOutput(true);
-            OutputStream os = con.getOutputStream();
-            os.write(POST_PARAMS.getBytes());
-            os.flush();
-            os.close();
-            // For POST only - END
+        // For POST only - START
+        con.setDoOutput(true);
+        OutputStream os = con.getOutputStream();
+        os.write(POST_PARAMS.getBytes());
+        os.flush();
+        os.close();
+        // For POST only - END
 
-            int responseCode = con.getResponseCode();
-            System.out.println("POST Response Code :: " + responseCode);
+        int responseCode = con.getResponseCode();
+        System.out.println("POST Response Code :: " + responseCode);
 
-            if (responseCode == HttpURLConnection.HTTP_OK) { //success
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        con.getInputStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
+        if (responseCode == HttpURLConnection.HTTP_OK) { //success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
 
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-                // print result
-                System.out.println(response.toString());
-            } else {
-                System.out.println("POST request not worked");
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
             }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("POST request not worked");
         }
+    }
 }
